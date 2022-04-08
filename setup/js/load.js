ff091efbc4a98a8f9e77bd1a20299153be687792
Fouyoufr <46448472+Fouyoufr@users.noreply.github.com>
2022-04-08 17:10:08 +0200

    alpha12
    Workin' on villain

--- setup/js/load.js
+++ setup/js/load.js
@@ -150,13 +150,15 @@ function villainDisplay(villainDisp,villain) {
         villainDisp.getElementsByClassName('name')[0].textContent=villains[villain.id].name;
         villainDisp.getElementsByClassName('phase')[0].textContent=villain.phase;
         villainDisp.getElementsByClassName('phase')[0].title=lang.BUTTONphase;
+        villainDisp.getElementsByClassName('phase')[0].onclick=function () {initChangePhase(villainDisp.id)}
         villainDisp.getElementsByClassName('life')[0].getElementsByClassName('value')[0].textContent=villain.life;
         ['confused','stunned','tough','retaliate','piercing','ranged'].forEach((statusName) => {
-            if (villain[statusName] === undefined || villain[statusName] !== '0') villainDisp.getElementsByClassName(statusName)[0].classList.add('off'); else villainDisp.getElementsByClassName(statusName)[0].classList.remove('off');})
+            if (villain[statusName] === undefined || villain[statusName] === '0') villainDisp.getElementsByClassName(statusName)[0].classList.add('off'); else villainDisp.getElementsByClassName(statusName)[0].classList.remove('off');})
         if (mainSchemes[villain.mainScheme.id] !== undefined) {
             //Affichage de la manigance principale du méchant
             mainDisp=villainDisp.getElementsByClassName('mainScheme')[0];
             mainDisp.getElementsByClassName('name')[0].textContent=mainSchemes[villain.mainScheme.id].name;
+            mainDisp.getElementsByClassName('name')[0].onclick=function () {changeVillainScheme(villainDisp.id)};
             mainDisp.getElementsByClassName('threat')[0].getElementsByClassName('value')[0].textContent=villain.mainScheme.current;
             mainDisp.getElementsByClassName('acceleration')[0].getElementsByClassName('value')[0].textContent=villain.mainScheme.acceleration;
             mainDisp.getElementsByClassName('max')[0].getElementsByClassName('value')[0].textContent=villain.mainScheme.max;}
@@ -284,14 +286,16 @@ function popupDisplay(title,intro,content,buttons,outro='',height='15%') {
     popup.getElementsByClassName('background')[0].style.height=height;}
 
 function initChangeVillain(villainDiv) {
-    //Popup de confirmation de changement du méchant de la partie en cours.
+    //Popup de confirmation de changement du méchant de la partie en cours (villainID = quel méchant 1 à 4)
     let villainId=villainDiv.charAt(villainDiv.length - 1)-1;
     let outro = (game.villains[villainId].phase != '1') ? lang.POPUPvillainOutro : '';
     let intro = lang.POPUPvillainIntro1 + villains[game.villains[villainId].id].name + '\'.<br/>' + lang.POPUPvillainIntro2;
     intro += (game.villains[villainId].sideSchemes.length > 0) ? lang.POPUPvillaintIntro3 : '.';
-    let changeVillainButtons='<button title="' + lang.BUTTONconfirm + '" id="initChangeVillainConfirm" style="display:none;" onclick="changeVillainScheme(document.querySelector(\'input[type=radio]:checked\').value);">' + lang.BUTTONconfirm + '</button><button title="' + lang.BUTTONcancel + '" onclick="document.getElementById(\'popup\').style.display=\'none\';">' + lang.BUTTONcancel + '</button>';
-    orderVillains=Object.entries(villains).sort((a,b) => a[1].name > b[1].name?1:-1);
-    let villainSelect='';
+    if (game.villains[villainId].id == 0) intro = lang.POPUPvillainIntro2b;
+    let changeVillainButtons='<button title="' + lang.BUTTONconfirm + '" id="initChangeVillainConfirm" style="display:none;" onclick="changeVillainScheme(\'' + villainDiv + '\',document.querySelector(\'input[type=radio]:checked\').value);">' + lang.BUTTONconfirm + '</button><button title="' + lang.BUTTONcancel + '" onclick="document.getElementById(\'popup\').style.display=\'none\';">' + lang.BUTTONcancel + '</button>';
+    let orderVillains=Object.entries(villains).sort((a,b) => a[1].name > b[1].name?1:-1);
+    //création d'un "dummy méchant" pour le cas de "Choisir le méchant"
+    let villainSelect=game.villains[villainId].id == 0 ? '<div style="display:none"><label class="on"></label></div>': '';
     for (let i in orderVillains) if (orderVillains[i][0] != 0) {
         //Affichage du formulaire de sélection des méchants :
         vilIndex=orderVillains[i][0];
@@ -299,7 +303,7 @@ function initChangeVillain(villainDiv) {
         if (vilIndex == game.villains[villainId].id) villainSelect += ' class="on"';
         villainSelect += '><input type="radio" id="villainselect_' + vilIndex + '" name="villainSelect" value="' + vilIndex +'"';
         if (vilIndex == game.villains[villainId].id) villainSelect += ' checked';
-        villainSelect +='><img alt="' + orderVillains[i][1].name + '" src="./images/villains/' + i + '.png">' + orderVillains[i][1].name + '</label></div>';}
+        villainSelect +='><img alt="' + orderVillains[i][1].name + '" src="./images/villains/' + vilIndex + '.png">' + orderVillains[i][1].name + '</label></div>';}
     popupDisplay(lang.BUTTONvillain,intro,villainSelect,changeVillainButtons,outro,'70%');
     var radios = document.querySelectorAll('input[type=radio][name="villainSelect"]');
     //Gestion de l'affichage du méchant sélectionné.
@@ -308,13 +312,44 @@ function initChangeVillain(villainDiv) {
         radio.parentElement.className='on';
         document.getElementById('initChangeVillainConfirm').style.display=radio.value == game.villains[villainId].id?'none':'block';}));}
 
-function changeVillainScheme (villainId=0) {
-    console.log(villainId);
-    
-}
+function changeVillainScheme (villainDiv,newVillainId=0) {
+    let villainId=villainDiv.charAt(villainDiv.length - 1)-1;
+    let intro='';
+    if (newVillainId === 0)  intro = game.villains[villainId].mainScheme === undefined || game.villains[villainId].mainScheme.id == 0 ? lang.POPUPmainIntroC:lang.POPUPmainIntroA1 + mainSchemes[game.villains[villainId].mainScheme.id].name + lang.POPUPmainIntroA2;
+    else intro = lang.POPUPmainIntroB1 + villains[newVillainId].name + lang.POPUPmainIntroB2;
+    let buttons='<button title="' + lang.BUTTONconfirm + '" id="changeMain" style="display:none;">' + lang.BUTTONconfirm + '</button><button title="' + lang.BUTTONcancel + '" onclick="document.getElementById(\'popup\').style.display=\'none\';">' + lang.BUTTONcancel + '</button>';
+    let orderMains=Object.entries(mainSchemes).sort((a,b) => a[1].name > b[1].name?1:-1);
+    let content=lang.POPUPmainContent + '<select name="mainScheme" id="mainSchemeChange" onchange="document.getElementById(\'changeMain\').style.display=this.value ==  ' + game.villains[villainId].mainScheme.id + ' || this.value == 0?\'none\':\'block\';"><option';
+    if (newVillainId == 0 || game.villains[villainId].mainScheme === undefined || game.villains[villainId].mainScheme.id == 0) content += ' selected';
+    content +=' value="0">' + mainSchemes[0].name + '</option>';
+    for (let i in orderMains) if (orderMains[i][0] != 0) {
+        //Affichage du menu de selection des Manigances principales :
+        content += '<option value="' + orderMains[i][0] + '"';
+        if (game.villains[villainId].mainScheme.id == orderMains[i][0]) content += ' selected';
+        content += '>' + orderMains[i][1].name + '</option>'
+    }
+    content += '</select>';
+    popupDisplay(lang.BUTTONmain,intro,content,buttons);}
 
 function adminPopup() {
     //Popup de saisie du mot de passe pour accès administratif
     let intro=lang.POPUPAdminIntro, content= lang.popupAdminContent;
     let buttons='<button title="' + lang.BUTTONconfirm + '">' + lang.BUTTONconfirm + '</button><button title="' + lang.BUTTONcancel + '" onclick="document.getElementById(\'popup\').style.display=\'none\';">' + lang.BUTTONcancel + '</button>';
-    popupDisplay(lang.MENUadmin,intro,content,buttons);}
+    popupDisplay(lang.MENUadmin,intro,content,buttons);
+    let refreshButton=document.createElement('button');
+    refreshButton.className='adminRefresh';
+    refreshButton.onclick=function () {localStorage.clear();location.reload();}
+    refreshButton.title=lang.BUTTONadminRefresh;
+    refreshButton.innerHTML='&nbsp;';
+    document.getElementById('popup').getElementsByClassName('title')[0].append(refreshButton);
+}
+
+function initChangePhase(villainDiv) {
+    let villainId=villainDiv.charAt(villainDiv.length - 1)-1;
+    let villain=villains[game.villains[villainId].id];
+    let newPhase=game.villains[villainId].phase;
+    newPhase++;
+    if (villain['life' + newPhase] === undefined) newPhase = 1;
+    console.log(newPhase);
+
+}
